b0374740bb825c7cb61728eb9bcc8dbf525cf07d
Anchusree <anchusreen@gmail.com>
2021-03-12 14:02:20 +0300

    Updates

---
+++ precache-manifest.f3127d3a2baf7de138b3fb1845b8b3f5.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "147df5e7005b8645fe9885ad4e1f8631",
+    "url": "/react-firebase-ecommerce-website/index.html"
+  },
+  {
+    "revision": "bad50ee4ebd8be54edd5",
+    "url": "/react-firebase-ecommerce-website/static/css/2.1cf52096.chunk.css"
+  },
+  {
+    "revision": "844aedbe769383249cb9",
+    "url": "/react-firebase-ecommerce-website/static/css/main.3c2ee93a.chunk.css"
+  },
+  {
+    "revision": "bad50ee4ebd8be54edd5",
+    "url": "/react-firebase-ecommerce-website/static/js/2.07ab642b.chunk.js"
+  },
+  {
+    "revision": "a4ddcbc2de5247cbaa24380fb5e79f82",
+    "url": "/react-firebase-ecommerce-website/static/js/2.07ab642b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "844aedbe769383249cb9",
+    "url": "/react-firebase-ecommerce-website/static/js/main.af96cb08.chunk.js"
+  },
+  {
+    "revision": "45983c8a93e3e5e1b145",
+    "url": "/react-firebase-ecommerce-website/static/js/runtime-main.3a0f17d7.js"
+  },
+  {
+    "revision": "80136942d360982098bee342c48b7837",
+    "url": "/react-firebase-ecommerce-website/static/media/defaultavatar.80136942.JPG"
+  },
+  {
+    "revision": "fb02edae3653eaa029635076fb5b6ea8",
+    "url": "/react-firebase-ecommerce-website/static/media/logo.fb02edae.png"
+  },
+  {
+    "revision": "5b21a1584b36184751e2a6d336c064fa",
+    "url": "/react-firebase-ecommerce-website/static/media/rate1.5b21a158.JPG"
+  },
+  {
+    "revision": "8963059d4515f4db6c01e75a083e11e0",
+    "url": "/react-firebase-ecommerce-website/static/media/rate2.8963059d.JPG"
+  },
+  {
+    "revision": "f42730c47fe924fbcf1ac0df688063fb",
+    "url": "/react-firebase-ecommerce-website/static/media/rate3.f42730c4.JPG"
+  },
+  {
+    "revision": "9b7b103f38e669aef342b83aba3fd70e",
+    "url": "/react-firebase-ecommerce-website/static/media/rate4.9b7b103f.JPG"
+  },
+  {
+    "revision": "194b6e75f99c6084399bc1978b205d2c",
+    "url": "/react-firebase-ecommerce-website/static/media/rate5.194b6e75.JPG"
+  },
+  {
+    "revision": "da3d2e269feab885e11ea26044f94b8a",
+    "url": "/react-firebase-ecommerce-website/static/media/sidebarbcg.da3d2e26.jpeg"
+  },
+  {
+    "revision": "e0f7e529b8372f3b679144de2c2cbb79",
+    "url": "/react-firebase-ecommerce-website/static/media/signbg.e0f7e529.jpg"
+  },
+  {
+    "revision": "13d0b92da21f939b5605c050002d1848",
+    "url": "/react-firebase-ecommerce-website/static/media/simplebcg.13d0b92d.jpg"
+  }
+]);

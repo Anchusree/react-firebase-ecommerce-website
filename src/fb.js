621380ff57dd6503a332ad1df621f28043b04394
Anchusree <68848197+Anchusree@users.noreply.github.com>
2021-09-25 23:29:24 +0300

    Update fb.js

--- src/fb.js
+++ src/fb.js
@@ -3,13 +3,7 @@ import "firebase/auth"
 import 'firebase/firestore'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBM9m4bdzyygTUe9xRjJsOWaV-KXAXrf40",
-    authDomain: "classiquebags-a072e.firebaseapp.com",
-    projectId: "classiquebags-a072e",
-    storageBucket: "classiquebags-a072e.appspot.com",
-    messagingSenderId: "770043207019",
-    appId: "1:770043207019:web:3e0f81ca683f6bf8a1802e",
-    measurementId: "G-1BYGFSFNJC"
+ 
   };
 
 
